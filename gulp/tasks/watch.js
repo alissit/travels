10cb98af655474a18c35b900518305e8001fbcd9
Alissa <alissit@gmail.com>
2018-02-16 16:02:02 +0000

    Complete large hero block and intro content block

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,29 @@
+var gulp 			= require("gulp"), //brings in the gulp files
+	watch 			= require("gulp-watch"),//brings in gulp-watch
+	browserSync		= require("browser-sync").create();//only want the create aspect of browser-sync
+
+
+
+
+gulp.task("watch", function(){
+
+		browserSync.init({
+			notify: false, /*stops the notify box from showing up*/
+			server:{
+				baseDir: "app"
+			}
+		});
+
+		watch("./app/index.html", function(){
+			browserSync.reload();
+		});
+
+		watch("./app/assets/styles/**/*.css", function(){
+		gulp.start("cssInject");
+	});
+});
+
+gulp.task("cssInject", ["styles"], function(){
+	return gulp.src("./app/temp/styles/styles.css")
+		.pipe(browserSync.stream());
+});
